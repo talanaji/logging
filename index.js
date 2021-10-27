9f2caf2566daa27171eee416abdc50225d8795c5
Tala Al-Naji <talanaji@gmail.com>
2021-10-27 13:54:18 +0200

    logging

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const winston = require('winston');
+
+// Imports the Google Cloud client library for Winston
+const { LoggingWinston } = require('@google-cloud/logging-winston');
+
+const loggingWinston = new LoggingWinston();
+
+// Create a Winston logger that streams to Stackdriver Logging
+// Logs will be written to: "projects/YOUR_PROJECT_ID/logs/winston_log"
+const logger = winston.createLogger({
+  level: 'debug',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.Console(),
+    // Add Stackdriver Logging
+    loggingWinston,
+  ],
+});
+
+module.exports = logger;
